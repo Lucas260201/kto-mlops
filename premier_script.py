a9d25f567a5082fe16856d95cbc0b23b3bcb1459
Lucas260201 <113217355+Lucas260201@users.noreply.github.com>
2024-02-05 14:15:10 +0000

    feat: Clean Code sur premier_script.py

--- premier_script.py
+++ premier_script.py
@@ -1,12 +1,13 @@
 import unittest
 
 """
-Count names with more than seven letters
+Count names with more than x letters
 """
-def names(prenoms):
+def count_long_names(prenoms: list[str]) ->str:
+    nombre_caracteres = 7
     more_than_seven = 0
     for prenom in prenoms:
-        if len(prenom) > 7:
+        if len(prenom) > nombre_caracteres:
             more_than_seven += 1
             print("Prenom supérieur à 7 : " + prenom)
         else:
@@ -16,7 +17,7 @@ def names(prenoms):
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
+        more_than_seven = count_long_names(prenoms=prenoms)
         self.assertEqual(more_than_seven, 4)
 
 if __name__ == '__main__':
